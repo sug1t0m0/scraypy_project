5f30f021767145b99b553bee1a0d4b7b5ea8c832
tomo_s <f15c017d@gmail.com>
2019-06-17 20:22:48 +0900

    typo修正

--- scraypy_base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker build -t scraypy-base-app .
-
-docker run -itd -v $(pwd)/app:/usr/src/app \
---name scraypy-base-app \
-scraypy-base-app
-
-docker exec -it scraypy-base-app scrapy startproject scraping .
